5d2166cc54a457f5dbb4cf75ea462ded78da820c
EC2 Default User <ec2-user@ip-10-0-1-182.ec2.internal>
2026-07-04 08:52:57 +0000

    temporary commit before merge

--- src/app.ts
+++ src/app.ts
@@ -116,3 +116,9 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 export default app;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+console.log('Server is listening on port ${PORT}');
+});
+
